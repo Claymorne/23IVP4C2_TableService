0ea897784bbcba89b4299b5b29cbc40d3accb6f2
Ray Trommelen <Ray.trommelen@hotmail.com>
2016-05-24 19:58:00 +0200

    1.1

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -5,12 +5,21 @@
  */
 package main;
 
+import javax.swing.JFrame;
+
+
 /**
  *
  * @author Ray
  */
 public class main {
         public static void main(String[] args) {
-        
+       JFrame Frame = new JFrame();
+       Frame.setSize(500, 400);
+       Frame.setDefaultCloseOperation(
+       JFrame.EXIT_ON_CLOSE);    
+       Frame.setTitle("Hartige Hap");
+       Frame.setContentPane(new presentation.ServiceGUI());
+       Frame.setVisible(true);
     }
 }
